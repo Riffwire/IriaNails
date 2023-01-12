196571b3811d6e1aeb97391f708b6b19ec54ecc1
Riffwire <nunofserra@gmail.com>
2023-01-12 19:15:38 -0100

    irianails mobile update

---
+++ scripts/javascript.js
@@ -0,0 +1,16 @@
+
+  function refreshpage() {
+    location.reload();
+  }
+
+  document.querySelectorAll('.card img').forEach(image =>{
+    image.onclick=()=>{
+
+    document.querySelector('.popup-img').style.display = 'block';
+    document.querySelector('.popup-img img').src=image.getAttribute('src');
+    }
+  });
+
+   document.querySelector('.popup-img span').onclick=()=>{
+    document.querySelector('.popup-img').style.display = 'none';
+  }
