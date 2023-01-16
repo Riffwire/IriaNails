6b0cbe3f4f3601a8f4325a267b5cb35169a703d1
Riffwire <nunofserra@gmail.com>
2023-01-16 14:51:20 -0100

    Add - load more layer

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,12 +1,30 @@
 
 function refreshpage() {
   location.reload();
+
 }
-function displayLoadMore(){
+function displayLoadMore() {
   document.querySelector('.about-layer').style.display = 'none';
   document.querySelector('.top-layer').style.display = 'none';
+  document.querySelector('.services-layer').style.display = 'none';
   document.querySelector('.gallery-layer').style.display = 'none';
   document.querySelector('.load-more-layer').style.display = 'grid';
+  window.scrollTo(99999, 0);
+  if (document.addEventListener) document.addEventListener("DOMContentLoaded", scrollBottom, true)
+  else if (window.attachEvent) window.attachEvent("onload", scrollBottom);
+}
+function HideLoadMore() {
+  var x = window.matchMedia("(max-width: 900px)")
+  if (x.matches) {
+    document.querySelector('.about-layer').style.display = 'grid';
+  } else {
+    document.querySelector('.about-layer').style.display = 'flex';
+  }
+
+  document.querySelector('.top-layer').style.display = 'flex';
+  document.querySelector('.gallery-layer').style.display = 'inline-block';
+  document.querySelector('.load-more-layer').style.display = 'none';
+  document.querySelector('.services-layer').style.display = 'flex';
 
 }
 
